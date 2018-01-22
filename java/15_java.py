7ce21797e8ada2f357265d2762981e08375caac6
liang.chen <liang.chen@funplus.com>
2018-01-22 14:19:26 +0800

    fix java bug

--- java/15_java.py
+++ java/15_java.py
@@ -8,81 +8,97 @@ import socket
 import time
 import json
 import copy
+import psutil
 
 class Resource():
-    def __init__(self, pid):
-        self.pid = pid
+    def __init__(self):
         self.config = '/data/open-falcon/cfg.json'
         with open(self.config) as cfg:
             self.data = json.load(cfg)
         self.host = self.data['hostname']
+        self.rs = []
 
-    def get_cpu_user(self):
-        cmd="cat /proc/" + str(self.pid)  +  "/stat |awk '{print $14+$16}'"
-        return os.popen(cmd).read().strip("\n")
+    def get_cpu_user(self, pid):
+        cmd="cat /proc/" + str(pid)  +  "/stat |awk '{print $14+$16}'"
+        try:
+            value = os.popen(cmd).read().strip("\n")
+        except Exception, e:
+            value = -1
+        return value
 
-    def get_cpu_sys(self):
-        cmd="cat /proc/" + str(self.pid)  +  "/stat |awk '{print $15+$17}'"
-        return os.popen(cmd).read().strip("\n")
+    def get_cpu_sys(self, pid):
+        cmd="cat /proc/" + str(pid)  +  "/stat |awk '{print $15+$17}'"
+        try:
+            value = os.popen(cmd).read().strip("\n")
+        except Exception, e:
+            value = -1
+        return value
 
-    def get_cpu_all(self):
-        cmd="cat /proc/" + str(self.pid)  +  "/stat |awk '{print $14+$15+$16+$17}'"
-        return os.popen(cmd).read().strip("\n")
+    def get_cpu_all(self, pid):
+        cmd="cat /proc/" + str(pid)  +  "/stat |awk '{print $14+$15+$16+$17}'"
+        try:
+            value = os.popen(cmd).read().strip("\n")
+        except Exception, e:
+            value = -1
+        return value
 
-    def get_mem(self):
-        cmd="cat /proc/" + str(self.pid)  +  "/status |grep VmRSS |awk '{print $2*1024}'"
-        return os.popen(cmd).read().strip("\n")
+    def get_mem(self, pid):
+        cmd="cat /proc/" + str(pid)  +  "/status |grep VmRSS |awk '{print $2*1024}'"
+        try:
+            value = os.popen(cmd).read().strip("\n")
+        except Exception, e:
+            value = -1
+        return value
 
-    def get_swap(self):
-        cmd="cat /proc/" + str(self.pid)  +  "/stat |awk '{print $(NF-7)+$(NF-8)}' "
-        return os.popen(cmd).read().strip("\n")
+    def get_swap(self, pid):
+        cmd="cat /proc/" + str(pid)  +  "/stat |awk '{print $(NF-7)+$(NF-8)}' "
+        try:
+            value = os.popen(cmd).read().strip("\n")
+        except Exception, e:
+            value = -1
+        return value
 
-    def get_fd(self):
-        cmd="cat /proc/" + str(self.pid)  +  "/status |grep FDSize |awk '{print $2}'"
-        return os.popen(cmd).read().strip("\n")
+    def get_fd(self, pid):
+        cmd="cat /proc/" + str(pid)  +  "/status |grep FDSize |awk '{print $2}'"
+        try:
+            value = os.popen(cmd).read().strip("\n")
+        except Exception, e:
+            value = -1
+        return value
 
     def run(self):
-        self.resources_d={
-            'java.cpu.user':[self.get_cpu_user,'COUNTER'],
-            'java.cpu.sys':[self.get_cpu_sys,'COUNTER'],
-            'java.cpu.all':[self.get_cpu_all,'COUNTER'],
-            'java.mem':[self.get_mem,'GAUGE'],
-            'java.swap':[self.get_swap,'GAUGE'],
-            'java.fd':[self.get_fd,'GAUGE']
-        }
+        pids = psutil.pids()
+        for pid in pids:
+            p = psutil.Process(pid)
+            try:
+                if p.name() == 'java':
+                    name = p.username()
+                    self.resources_d={
+                        'java.cpu.user':[self.get_cpu_user,'COUNTER'],
+                        'java.cpu.sys':[self.get_cpu_sys,'COUNTER'],
+                        'java.cpu.all':[self.get_cpu_all,'COUNTER'],
+                        'java.mem':[self.get_mem,'GAUGE'],
+                        'java.swap':[self.get_swap,'GAUGE'],
+                        'java.fd':[self.get_fd,'GAUGE']
+                    }
 
-        if not os.path.isdir("/proc/" + str(self.pid)):
-            return
+                    if not os.path.isdir("/proc/" + str(pid)):
+                        return
 
-        output = []
-        for resource in  self.resources_d.keys():
-                t = {}
-                t['endpoint'] = self.host
-                t['timestamp'] = int(time.time())
-                t['step'] = 60
-                t['counterType'] = self.resources_d[resource][1]
-                t['metric'] = resource
-                t['value']= self.resources_d[resource][0]()
-                t['tags'] = 'name=tomcat'
-                output.append(t)
+                    for resource in  self.resources_d.keys():
+                            t = {}
+                            t['endpoint'] = self.host
+                            t['timestamp'] = int(time.time())
+                            t['step'] = 60
+                            t['counterType'] = self.resources_d[resource][1]
+                            t['metric'] = resource
+                            t['value']= self.resources_d[resource][0](pid)
+                            t['tags'] = 'name=%s' % name
+                            self.rs.append(t)
+            except Exception, e:
+                continue
 
-        return output
-
-    def dump_data(self):
-        return json.dumps()
+        print json.dumps(self.rs)
 
 if __name__ == "__main__":
-    import psutil
-    pids = psutil.pids()
-    java_pid = ''
-    for pid in pids:
-        p = psutil.Process(pid)
-        name = p.name()
-        if name == 'java':
-            for i in p.cmdline():
-                if "tomcat" in i:
-                    java_pid = pid
-                    break
-    d = Resource(java_pid).run()
-    if d:
-        print json.dumps(d)
+    Resource().run()
