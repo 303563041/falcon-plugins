1a250bdaa8601474c286af4798beb8401076dc87
liang.chen <liang.chen@funplus.com>
2018-03-19 16:41:04 +0800

    fix bug

--- monitor/60_monitor.py
+++ monitor/60_monitor.py
@@ -6,102 +6,97 @@ import json
 import requests
 import time
 
-def get_statistics(host, port):
-    ''' get open falcon statistics '''
-    uri = "/counter/all"
-    url = host + ":" + port + uri
-    try:
-        response = requests.get(url)
-    except Exception as e:
-        print "Url request error: {0}".format(e)
+class Resource:
 
-    return json.loads(response.text)
+    def __init__(self):
+        self.p = []
+        self.ts = int(time.time())
+        self.step = 60
+        self.metrics = [
+            ("transfer", "http://localhost", "6060", "RecvCnt"),
+            ("transfer", "http://localhost", "6060", "SendToGraphCnt"),
+            ("transfer", "http://localhost", "6060", "SendToJudgeCnt"),
+            ("graph", "http://localhost", "6071", "GraphRpcRecvCnt"),
+            ("graph", "http://localhost", "6071", "GraphQueryCnt")
+        ]
 
-def main():
-    p = []
-    ts = int(time.time())
-    metrics = [
-        {
-            'name': 'transfer',
-            'port': '6060',
-            'host': 'http://localhost'
-        },
-        {
-            'name': 'graph',
-            'port': '6071',
-            'host': 'http://localhost'
-        }
-    ]
 
-    for metric in metrics:
-        name = metric["name"]
-        host = metric["host"]
-        port = metric["port"]
+    def get_statistics(self, host, port):
+        ''' get open falcon statistics '''
+        uri = "/counter/all"
+        url = host + ":" + port + uri
+        try:
+            response = requests.get(url)
+        except Exception as e:
+            print "Url request error: {0}".format(e)
 
-        response = get_statistics(host, port)
-        if name == "transfer":
-            qps = ['RecvCnt', 'SendToGraphCnt', 'SendToJudgeCnt']
-            if response["msg"] == "success":
-                for m in qps:
+        return json.loads(response.text)
+
+    def run(self):
+        for metric in self.metrics:
+            name = metric[0]
+            host = metric[1]
+            port = metric[2]
+            qps = metric[3]
+
+            response = self.get_statistics(host, port)
+            if name == "transfer":
+                if response["msg"] == "success":
                     for i in response["data"]:
-                        if i["Name"] == m:
+                        if i["Name"] == qps:
                             value = i["Qps"]
                     datapoint = {
-                        'metric': '{0}.{1}.Qps'.format(name, m),
+                        'metric': '{0}.{1}.Qps'.format(name, qps),
                         'endpoint': '172.31.32.191-open-falcon',
-                        'timestamp': ts,
-                        'step': '60',
+                        'timestamp': self.ts,
+                        'step': self.step,
                         'value': value,
                         'counterType': 'COUNTER',
                         'tags': ''
                     }
-                    p.append(datapoint)
-            else:
-                value = 0
-                for m in qps:
+                    self.p.append(datapoint)
+                else:
+                    value = 0
                     datapoint = {
-                        'metric': '{0}.{1}.Qps'.format(name, m),
+                        'metric': '{0}.{1}.Qps'.format(name, qps),
                         'endpoint': '172.31.32.191-open-falcon',
-                        'timestamp': ts,
-                        'step': '60',
+                        'timestamp': self.ts,
+                        'step': self.step,
                         'value': value,
                         'counterType': 'COUNTER',
                         'tags': ''
                     }
-                    p.append(datapoint)
-        elif name == "graph":
-            qps = ['GraphRpcRecvCnt', 'GraphQueryCnt']
-            if response:
-                for m in qps:
+                    self.p.append(datapoint)
+            elif name == "graph":
+                if response:
                     for i in response:
-                        if i["Name"] == m:
+                        if i["Name"] == qps:
                             value = i["Qps"]
                     datapoint = {
-                        'metric': '{0}.{1}.Qps'.format(name, m),
+                        'metric': '{0}.{1}.Qps'.format(name, qps),
                         'endpoint': '172.31.32.191-open-falcon',
-                        'timestamp': ts,
-                        'step': '60',
+                        'timestamp': self.ts,
+                        'step': self.step,
                         'value': value,
                         'counterType': 'COUNTER',
                         'tags': ''
                     }
-                    p.append(datapoint)
-            else:
-                value = 0
-                for m in qps:
+                    self.p.append(datapoint)
+                else:
+                    value = 0
                     datapoint = {
-                        'metric': '{0}.{1}.Qps'.format(name, m),
+                        'metric': '{0}.{1}.Qps'.format(name, qps),
                         'endpoint': '172.31.32.191-open-falcon',
-                        'timestamp': ts,
-                        'step': '60',
+                        'timestamp': self.ts,
+                        'step': self.step,
                         'value': value,
                         'counterType': 'COUNTER',
                         'tags': ''
                     }
-                    p.append(datapoint)
+                    self.p.append(datapoint)
 
-    print json.dumps(p)
+        print json.dumps(self.p)
 
 if __name__ == "__main__":
-    main()
+    Resource().run()
 
